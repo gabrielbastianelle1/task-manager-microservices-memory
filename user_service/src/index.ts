0ef71918a08c896692877ef8df6407fe0181f31d
Gabriel Bastianelle <gabrielbastianelle@pop-os.localdomain>
2023-04-21 02:16:52 +0100

    fixed cors

--- user_service/src/index.ts
+++ user_service/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import router from "./route/route";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(3000, () => {
